5b4722b3782558dfed108ad23fc8cfe1ae4b11fc
Balram <a-dag-a@github.com>
2023-02-05 23:22:55 +0530

    Some more updates

--- meta-unit-cell/meta-unit-cell.py
+++ meta-unit-cell/meta-unit-cell.py
@@ -1,7 +1,9 @@
 import meep as mp
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.constants import c
 
+a = 1e-6
 # Atom-specific paramaters
 # |<------------ srr_sq_outer ------------->|
 #  ___________________________________  ______
@@ -24,11 +26,18 @@ import matplotlib.pyplot as plt
 #                 srr_gap
 
 def main(args):
-    resolution = args.resolution
+    # resolution = args.resolution
     no_sim = args.no_sim
+    print(f'no_sim flag is {no_sim}')    
+    
+
+    # Parse the frequency range for the input pulse
+    # a = 1e-6
+    # realLife2mp_freq = lambda f: f/(c/a)
+    # src_fcen = realLife2mp_freq(args.fcen)
+    # src_df = realLife2mp_freq(args.df)
 
     # GEOMETRY
-
     d_padding = 10
     d_pml = 2
 
@@ -36,7 +45,8 @@ def main(args):
     srr_track = 1
     srr_sq_inner = srr_sq_outer-2*srr_track
     srr_gap = srr_track/2
-    # resolution = 10
+    resolution = 10
+    
 
     cell_a = srr_sq_outer+srr_track
 
@@ -47,6 +57,8 @@ def main(args):
         mp.Block(size=mp.Vector3(srr_gap,srr_track,0),center=mp.Vector3(0,-srr_sq_outer/2+srr_track/2,0),material=mp.air),
         mp.Block(size=mp.Vector3(srr_track,2*srr_gap,0),center=mp.Vector3(-srr_sq_outer/2+srr_track/2,0,0),material=mp.air)
     ]
+    # if(srr_gap!=0):
+    #     geometry.append(mp.Block(size=mp.Vector3(srr_track,2*srr_gap,0),center=mp.Vector3(-srr_sq_outer/2+srr_track/2,0,0),material=mp.air))
 
     # SOURCES
     src_fcen = 3
@@ -82,22 +94,19 @@ def main(args):
         sim.dump(dirname='./sim_dump')
     else:
         sim.load(dirname='./sim_dump')
-        
-
+    
+    print(f'resolution is {resolution}')
     # PLOTTING THE VECTOR FIELD
     f1 = plt.figure()
     vol_srr = mp.Volume(size=mp.Vector3(cell_a,cell_a,0),center=mp.Vector3(0,0,0))
     sim.plot2D(fields=mp.Ex, output_plane=vol_srr)
     # plt.show()
 
-    # def showVectorField2D(volume,vector='E'):
-    #     # returns a plot object
-    #     plt.
-
     f2 = plt.figure()
     arrowX = sim.get_array(component=mp.Ex)[:,:,120]
     arrowY = sim.get_array(component=mp.Ey)[:,:,120]
     plt.quiver(arrowX,arrowY)
+
     plt.show()
 
 if __name__ == '__main__':
@@ -105,12 +114,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--resolution", type=int, default=10)
     parser.add_argument("--no-sim", type=int, default=0)
+    # parser.add_argument("--fcen", type=float, default=3*(c/a))
+    # parser.add_argument("--df", type=float, default=1.5*(c/a))
 
     args = parser.parse_args()
     main(args)
-
-#TODO: main(args, srr_params), srr_params can be loaded from a config yaml
-#TODO: Sanity checks on arguments
+    #TODO: main(args, srr_params), srr_params can be loaded from a config yaml
+    #TODO: Sanity checks on arguments
 
 # The quiver plot is misaligned. These corrections need to be done
 # Flip along Y axis

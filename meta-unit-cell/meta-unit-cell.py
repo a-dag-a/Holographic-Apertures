95375854e0bb7a435a2d83fadfef85ea9b8b63f5
Balram <a-dag-a@github.com>
2023-02-06 01:03:21 +0530

    Save checkpoint

--- meta-unit-cell/meta-unit-cell.py
+++ meta-unit-cell/meta-unit-cell.py
@@ -2,6 +2,7 @@ import meep as mp
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.constants import c
+import pdb
 
 a = 1e-6
 # Atom-specific paramaters
@@ -38,32 +39,36 @@ def main(args):
     # src_df = realLife2mp_freq(args.df)
 
     # GEOMETRY
-    d_padding = 10
-    d_pml = 2
+    # Main geometric parameters =======
+    space_factor = 140/5 # multplicative factor for 5 um -> 140 um ring edge length
+    d_padding = 10#*(140/5) # NOT SCALING
+    d_pml = 2#*(140/5) # NOT SCALING
 
-    srr_sq_outer = 5
-    srr_track = 1
+    srr_sq_outer = 5*space_factor
+    srr_track = 1*space_factor
+    srr_gap = 0*srr_track/2
+
     srr_sq_inner = srr_sq_outer-2*srr_track
-    srr_gap = srr_track/2
-    resolution = 10
-    
+    # resolution = 10/space_factor
+    resolution = 0.5
+    src_fcen = 3/space_factor
+    src_df = 1.5/space_factor
+    run_time = 30*space_factor
 
+    # =================================
     cell_a = srr_sq_outer+srr_track
-
     cell = mp.Vector3(cell_a,cell_a,2*d_padding+2*d_pml)
     geometry = [
         mp.Block(size=mp.Vector3(srr_sq_outer,srr_sq_outer,0),center=mp.Vector3(0,0,0),material=mp.metal),
         mp.Block(size=mp.Vector3(srr_sq_inner,srr_sq_inner,0),center=mp.Vector3(0,0,0),material=mp.air),
-        mp.Block(size=mp.Vector3(srr_gap,srr_track,0),center=mp.Vector3(0,-srr_sq_outer/2+srr_track/2,0),material=mp.air),
-        mp.Block(size=mp.Vector3(srr_track,2*srr_gap,0),center=mp.Vector3(-srr_sq_outer/2+srr_track/2,0,0),material=mp.air)
+        # mp.Block(size=mp.Vector3(srr_gap,srr_track,0),center=mp.Vector3(0,-srr_sq_outer/2+srr_track/2,0),material=mp.air),
+        # mp.Block(size=mp.Vector3(srr_track,2*srr_gap,0),center=mp.Vector3(-srr_sq_outer/2+srr_track/2,0,0),material=mp.air)
     ]
-    # if(srr_gap!=0):
-    #     geometry.append(mp.Block(size=mp.Vector3(srr_track,2*srr_gap,0),center=mp.Vector3(-srr_sq_outer/2+srr_track/2,0,0),material=mp.air))
+    if(srr_gap!=0):
+        geometry.append(mp.Block(size=mp.Vector3(srr_track,2*srr_gap,0),center=mp.Vector3(-srr_sq_outer/2+srr_track/2,0,0),material=mp.air))
 
     # SOURCES
-    src_fcen = 3
-    src_df = 1.5
-
+    
     sources = [
         mp.Source(
             src=mp.GaussianSource(src_fcen,src_df),
@@ -85,30 +90,38 @@ def main(args):
     )
 
     if(not(no_sim)):
-        sim.run(until=30)
-
-        # sim.plot2D(fields=mp.Ez)
-        # plt.show()
+        sim.run(until=run_time)
 
         # DUMP SIMULATION STATE AND DATA
         sim.dump(dirname='./sim_dump')
     else:
+        # LOAD SIMULATION DATA
         sim.load(dirname='./sim_dump')
     
     print(f'resolution is {resolution}')
     # PLOTTING THE VECTOR FIELD
     f1 = plt.figure()
-    vol_srr = mp.Volume(size=mp.Vector3(cell_a,cell_a,0),center=mp.Vector3(0,0,0))
-    sim.plot2D(fields=mp.Ex, output_plane=vol_srr)
+    plotZSlice(sim=sim,z=0)
+    # vol_srr = mp.Volume(size=mp.Vector3(cell_a,cell_a,0),center=mp.Vector3(0,0,0))
+    # sim.plot2D(fields=mp.Ex, output_plane=vol_srr)
     # plt.show()
 
     f2 = plt.figure()
-    arrowX = sim.get_array(component=mp.Ex)[:,:,120]
-    arrowY = sim.get_array(component=mp.Ey)[:,:,120]
+    num_z = int(cell.z*resolution)
+    print(f'num_z is {num_z}')
+    temp = sim.get_array(component=mp.Ex)
+    print(f'SHAPE OF EX IS {np.shape(temp)}')
+    arrowX = sim.get_array(component=mp.Ex)[:,:,int(num_z/2)]
+    arrowY = sim.get_array(component=mp.Ey)[:,:,int(num_z/2)]
     plt.quiver(arrowX,arrowY)
 
     plt.show()
+    pdb.set_trace()  
 
+def plotZSlice(sim, z=0):
+    vol_srr = mp.Volume(size=mp.Vector3(sim.cell_size.x,sim.cell_size.y,0),center=mp.Vector3(0,0,z))
+    sim.plot2D(fields=mp.Ex, output_plane=vol_srr)
+    
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
